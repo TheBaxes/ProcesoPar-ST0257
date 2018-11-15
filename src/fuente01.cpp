46ecc70fed32008046848939ede6b1f8343fcb4a
TheBaxes <sebas1475@gmail.com>
2018-11-15 01:41:51 -0500

    24 lines per function

--- src/fuente01.cpp
+++ src/fuente01.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <cerrno>
 #include <pthread.h>
 #include <sys/types.h>
@@ -8,81 +7,73 @@
 
 using namespace std;
 
-void* hilo(void *args) {
-  ProcesoPar_t *procesoPar = (ProcesoPar_t *) args;
-
-  char msg[80];
-  int result;
-    
-  while(true){
-    result = read(procesoPar->tuberia_out[0], msg, sizeof(msg)-1);
-    msg[result] = '\0';
-    
-    if (result < 0) {
-      cerr << "Error: " << errno << " at reading pipe" << endl;
-      int *ret = new int;
-      *ret = errno;
-      pthread_exit(ret);
-    } else if (result == 0) {
-      int *ret = new int;
-      *ret = 0;
-      pthread_exit(ret);
-    }
-    
-    if (procesoPar->f){
-      sem_wait(&procesoPar->mutex);
-      procesoPar->f(msg, sizeof(msg));
-      sem_post(&procesoPar->mutex);
-    }    
-  }
-  int *ret = new int;
-  *ret = 0;
-  return ret;
-}
+int abrirHijo(ProcesoPar_t *procesoPar, const char *nombreArchivoEjecutable,
+	      char *const *listaLineaComando,
+	      char *const *env);
+void* hilo(void *args);
 
 ProcesoPar_t *lanzarProcesoPar(const char *nombreArchivoEjecutable,
 			       char *const *listaLineaComando,
 			       char *const *env){
   ProcesoPar_t *procesoPar = new ProcesoPar_t();
-
   sem_init(&procesoPar->mutex, 0, 1);
-  
   procesoPar->tuberia_in = new int[2];
   procesoPar->tuberia_out = new int[2];
-  
-  if (pipe(procesoPar->tuberia_in) != 0) return nullptr;
-  if (pipe(procesoPar->tuberia_out) != 0) return nullptr;
-  procesoPar->hijo = fork();
-  
-  if (procesoPar->hijo == -1) {
-    return nullptr;
-  }
-
+  if (pipe(procesoPar->tuberia_in) != 0 ||
+      pipe(procesoPar->tuberia_out) != 0) return nullptr;
+  if ((procesoPar->hijo = fork()) == -1) return nullptr;
   if (procesoPar->hijo == 0) {
-    
-    if (dup2(procesoPar->tuberia_in[0], 0) != 0 ||
-    	close(procesoPar->tuberia_in[0]) != 0 ||
-    	close(procesoPar->tuberia_in[1]) != 0) {
-      exit(-1);
-    }
-    if (dup2(procesoPar->tuberia_out[1], 1) != 1 ||
-    	close(procesoPar->tuberia_out[1]) != 0 ||
-    	close(procesoPar->tuberia_out[0]) != 0) {
-      exit(-1);
-    }
-    execve(nombreArchivoEjecutable, listaLineaComando, env);
-    exit(-1);
+    if (abrirHijo(procesoPar, nombreArchivoEjecutable,
+		  listaLineaComando, env) < 0) _exit(-1);
   } else {
-    
     int error = pthread_create(&procesoPar->thread, NULL, hilo, procesoPar);
     if (error != 0) {
       errno = error;
       return nullptr;
     }
     if (close(procesoPar->tuberia_in[0]) != 0 ||
-	close(procesoPar->tuberia_out[1]) != 0) {
-      return nullptr;
-    }
+	close(procesoPar->tuberia_out[1]) != 0) return nullptr;
   }
   return procesoPar;
+}
+
+int abrirHijo(ProcesoPar_t *procesoPar, const char *nombreArchivoEjecutable,
+	      char *const *listaLineaComando,
+	      char *const *env) {
+  if (dup2(procesoPar->tuberia_in[0], 0) != 0 ||
+      close(procesoPar->tuberia_in[0]) != 0 ||
+      close(procesoPar->tuberia_in[1]) != 0) {
+    return -1;
+    }
+  if (dup2(procesoPar->tuberia_out[1], 1) != 1 ||
+      close(procesoPar->tuberia_out[1]) != 0 ||
+      close(procesoPar->tuberia_out[0]) != 0) {
+    return -1;
+    }
+  execve(nombreArchivoEjecutable, listaLineaComando, env);
+  return -1;
+}
+
+void* hilo(void *args) {
+  ProcesoPar_t *procesoPar = (ProcesoPar_t *) args;
+  char msg[80];
+  int result;
+  int *ret = new int;
+  *ret = 0;
+  while(true){
+    result = read(procesoPar->tuberia_out[0], msg, sizeof(msg)-1);
+    msg[result] = '\0';
+    if (result < 0) {
+      *ret = errno;
+      pthread_exit(ret);
+    } else if (result == 0) {
+      pthread_exit(ret);
+    }
+    if (procesoPar->f){
+      sem_wait(&procesoPar->mutex);
+      procesoPar->f(msg, sizeof(msg));
+      sem_post(&procesoPar->mutex);
+    }    
+  }
+  return ret;
 }
--- src/fuente02.cpp
+++ src/fuente02.cpp
@@ -9,20 +9,14 @@
 using namespace std;
 
 int destruirProcesoPar(ProcesoPar_t *procesoPar) {
-  int *retval[1];
-  time_t start, now;
-  pid_t status;
-  
+  int *retval[1]; time_t start, now; pid_t status;
   if (close(procesoPar->tuberia_in[1]) != 0) return -1;
-
-  time(&start);
-  time(&now);
+  time(&start); time(&now);
   while ((status = waitpid(procesoPar->hijo, NULL, WNOHANG)) == 0 &&
 	 difftime(start, now) < 30) {
     usleep(10000);
     time(&now);
   }
-  
   if (status == 0) {
     kill(procesoPar->hijo, SIGKILL);
     pthread_kill(procesoPar->thread, SIGKILL);
@@ -30,7 +24,6 @@ int destruirProcesoPar(ProcesoPar_t *procesoPar) {
     errno = ETIMEDOUT;
     return -1;
   }
-  
   if (pthread_join(procesoPar->thread, (void**) &retval[0]) != 0) return -1;
   if (close(procesoPar->tuberia_out[0]) != 0) return -1;
   if (*retval[0] != 0) {
